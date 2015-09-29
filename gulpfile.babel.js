48ff29cebbe7c92531294e5147b1869488b7db3f
ericsoco <ericsoco@yahoo-inc.com>
2015-09-29 11:13:25 -0700

    Document gulpfile.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -8,9 +8,18 @@ import watchify from 'watchify';
 
 const $ = gulpLoadPlugins();
 
-const examplesSrc = './examples';
-const examplesBuild = './examples/build';
+/**
+ * List of all external dependencies,
+ * which will be bundled separately from
+ * example application files and @panorama/toolkit.
+ */
+const dependencies = [
+	'react'
+];
 
+/**
+ * Pretty-print bundle build timings.
+ */
 const logTime = fileName => {
 	const name = $.util.colors.cyan(fileName);
 	return ms => {
@@ -21,58 +30,66 @@ const logTime = fileName => {
 	};
 };
 
-const bundleExample = (b, fileName) => {
+/**
+ * Given a browserify instance, `bundle()`s up files
+ * and writes them to the specified `fileName`
+ * within ./examples/build.
+ */
+const bundleExamples = (b, fileName) => {
 	return b.bundle()
 	.pipe(source(fileName))
 	.pipe(gulp.dest('./examples/build'));
 };
 
+/**
+ * Wraps a browserify instance in watchify,
+ * which logs build step timings
+ * and rebuilds ./examples/build when
+ * files within ./examples change.
+ */
 const watchExample = (b, fileName) => {
 	const w = watchify(b).on('time', logTime(fileName));
-	const bundle = () => bundleExample(w, fileName);
+	const bundle = () => bundleExamples(w, fileName);
 	w.on('update', bundle);
 	return bundle();
 };
 
-const simpleBundler = browserify(examplesSrc + '/index.jsx', watchify.args)
-	.external('react')
+/**
+ * browserify instance to package files in ./examples,
+ * specifying dependencies and @panorama/toolkit as
+ * external dependencies (and excluding from this build).
+ */
+const exampleBundler = browserify('./examples' + '/index.jsx', watchify.args)
+	.external(dependencies)
 	.external('@panorama/toolkit')
 	.transform(babelify);
 
+/**
+ * Bundle all external dependencies into one file,
+ * to allow end users take advantage of browser caching.
+ */
 gulp.task('examples:deps', () => {
 	const bundler = browserify()
-		.require('react')
-	return bundleExample(bundler, 'dependencies.js');
+		.require(dependencies)
+	return bundleExamples(bundler, 'dependencies.js');
 });
 
+/**
+ * Bundle the files in ./src into a lib bundle that can be included
+ * in other projects, via `import * from @panorama/toolkit`.
+ * Include React as an external (not bundled) dependency.
+ */
 gulp.task('examples:lib', () => {
-
-	// 
-	// TODO: TUESDAY
-	// almost there. but @panorama/toolkit is not getting built into lib.js properly;
-	// logging out imported Panorama shows an object with Legend and Punchcard objects, but they're undefined.
-	// how does ./examples/build/lib.js even get built?
-	// where is it pulling from?
-	// i'm guessing the following .require('./', { expose: '@panorama/toolkit' })
-	// follows the dependency chain from the root to put together @panorama/toolkit,
-	// (in english: require ./ and below, and expose as @panorama/toolkit)
-	// and it should just be pulling from ./src/main.js.
-	// not sure...
-	//
-
-	/*
 	const bundler = browserify()
-		.external('react')
-		.require('./', { expose: '@panorama/toolkit' })
-	return bundleExample(bundler, 'lib.js');
-	*/
-
-	const bundler = browserify()
-		.external('react')
+		.external(dependencies)
 		.require('./src/main.js', { expose: '@panorama/toolkit' })
-	return bundleExample(bundler, 'lib.js');
+	return bundleExamples(bundler, 'lib.js');
 });
 
+/**
+ * Run a webserver for local testing,
+ * pointed at ./examples.
+ */
 gulp.task('examples:server', () => gulp
 	.src('./examples')
 	.pipe($.webserver({
@@ -81,20 +98,43 @@ gulp.task('examples:server', () => gulp
 	}))
 );
 
-gulp.task('examples:app', () => bundleExample(simpleBundler, 'index.js'));
+/**
+ * Rebuild examples without rebuilding
+ * @panorama/toolkit or dependencies.
+ */
+gulp.task('examples:app', () => {
+	bundleExamples(exampleBundler, 'index.js')
+});
 
+/**
+ * Watch for changes in ./lib; on change,
+ * rebuild lib and dependencies in examples.
+ */
 gulp.task('watch:examples:lib', ['examples:deps'], () => {
 	return gulp.watch('./lib/**', ['examples:lib']);
 });
 
+/**
+ * Watch for changes in ./examples; on change,
+ * rebuild everything within ./examples.
+ */
 gulp.task('watch:examples:app', ['examples:deps', 'examples:lib'], () => {
-	return watchExample(simpleBundler, 'index.js');
+	return watchExample(exampleBundler, 'index.js');
 });
 
+/**
+ * Run a local webserver and watch
+ * for changes in ./lib and ./examples.
+ */
 gulp.task('watch:examples', [
 	'examples:server',
 	'watch:examples:lib',
 	'watch:examples:app'
 ]);
 
+/**
+ * Build everything in ./examples:
+ * example application files, lib, and dependencies.
+ * Does not launch a watcher or webserver.
+ */
 gulp.task('default', ['examples:deps', 'examples:lib', 'examples:app']);
