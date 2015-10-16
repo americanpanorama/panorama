36f7e085ed6429e0350c5077e34efba7f001de01
ericsoco <ericsoco@yahoo-inc.com>
2015-10-16 13:15:52 -0700

    publish: Bit more styling on examples page
    generated from commit a8b8f18a31b41aa673034d9838bde74f5e78d787

--- index.jsx
+++ index.jsx
@@ -3,16 +3,19 @@ import * as React from 'react';
 import LegendExample from './components/example-legend.jsx';
 import BarchartExample from './components/example-barchart.jsx';
 import CholoplethExample from './components/example-cholopleth.jsx';
+import PunchcardExample from './components/example-punchcard.jsx';
 
 const examples = (
 	<div>
 		<h1>Panorama Toolkit examples</h1>
 		<h2>Legend</h2>
 		<LegendExample />
-    <h2>Barchart</h2>
-    <BarchartExample />
-    <h2>Choropleth</h2>
-    <CholoplethExample />
+		<h2>Barchart</h2>
+		<BarchartExample />
+		<h2>Cholopleth</h2>
+		<CholoplethExample />
+		<h2>Punchcard</h2>
+		<PunchcardExample />
 	</div>
 );
 
