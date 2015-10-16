11803eb0f9ee79ed33b29b0f895729e49815b585
ericsoco <ericsoco@yahoo-inc.com>
2015-10-16 13:02:55 -0700

    Remove comments

--- examples/components/example-punchcard.jsx
+++ examples/components/example-punchcard.jsx
@@ -10,10 +10,6 @@ export default class PunchcardExample extends React.Component {
 
 	render () {
 
-		// TODO: friday: finish this example,
-		// push and link to source in comment for sean,
-		// npm version and publish and include in canals.
-
 		let punchcardData = require('../data/punchcard.json');
 
 		return (
