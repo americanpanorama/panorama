d6177ab9d3755d2230d99c9b9f829fc74ec1d4f5
Sean Connelley <connelley@stamen.com>
2015-10-16 11:29:55 -0400

    publish: Initial D3 cholopleth component
    generated from commit 1221a46fa198add8f6507e3f1ded60f276f4f6a6

--- src/main.js
+++
@@ -1,5 +0,0 @@
-// exports for use of @panorama/toolkit as a node module
-
-export Legend from './Legend/Legend.jsx';
-export Punchcard from './Punchcard/Punchcard.jsx';
-export PanoramaChart from './PanoramaChart/PanoramaChart.jsx';
