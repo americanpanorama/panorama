1bd77d2a79314777de89df770817539c4a1c8474
ericsoco <eric@transmote.com>
2015-12-15 11:56:41 -0800

    Add map state hashing to HashManager

--- src/HashManager/HashManager.js
+++ src/HashManager/HashManager.js
@@ -3,6 +3,7 @@ import { EventEmitter } from 'events';
 const HashManager = (function () {
 
   const EVENT_HASH_CHANGED = 'hashChanged';
+  const MAP_KEY = 'loc';
   
   let hashManager = {},
     state = {};
@@ -14,6 +15,7 @@ const HashManager = (function () {
   window.addEventListener('hashchange', onHashChange);
 
   function updateHash (newState) {
+
     let mergedState = Object.assign({}, state, newState);
 
     // remove null/undefined values
@@ -27,6 +29,7 @@ const HashManager = (function () {
     if (document.location.hash !== hash) {
       document.location.replace(hash);
     }
+
   }
 
   /**
@@ -36,33 +39,83 @@ const HashManager = (function () {
    * @return {String|Object}    If `key` is passed, returns the value stored at that key; otherwise, returns all state in the hash.
    */
   function getState (key=null) {
+
     if (key) {
       return state[key];
     } else {
       return Object.assign({}, state);
     }
+
   }
 
   function setState (val, silent) {
+
     state = val;
     if (!silent) {
       hashManager.emit(EVENT_HASH_CHANGED, Object.assign({}, state));
     }
+
   }
 
   function onHashChange () {
+
     setState(parseHash(window.location.hash));
+
   }
 
   function parseHash (hash) {
+
     // Split into `&`-delimited parts and store as key-value pairs
     let hashState = hash.replace(/^#\/?|\/$/g, '').split('&').reduce((obj, pair) => {
       pair = pair.split('=');
+      if (pair[0] === MAP_KEY) {
+        pair[1] = parseMapCenterAndZoom(pair[1]);
+      }
       obj[pair[0]] = pair[1];
       return obj;
     }, {});
 
     return hashState;
+
+  }
+
+  function parseMapCenterAndZoom (args) {
+
+    let args = args.split('/');
+    if (args.length == 3) {
+
+      let zoom = parseInt(args[0], 10),
+        lat = parseFloat(args[1]),
+        lon = parseFloat(args[2]);
+
+      if (isNaN(zoom) || isNaN(lat) || isNaN(lon)) {
+        return false;
+      } else {
+
+        return {
+          center: [lat, lon],
+          zoom: zoom
+        };
+
+      }
+    } else {
+      return false;
+    }
+
+  }
+
+  function formatMapCenterAndZoom (map) {
+
+    let center = map.getCenter(),
+      zoom = map.getZoom(),
+      precision = Math.max(0, Math.ceil(Math.log(zoom) / Math.LN2));
+
+    return [
+      zoom,
+      center.lat.toFixed(precision),
+      center.lng.toFixed(precision)
+    ].join('/');
+
   }
 
   // Establish initial state
@@ -70,6 +123,7 @@ const HashManager = (function () {
 
   // Public interface
   hashManager.EVENT_HASH_CHANGED = EVENT_HASH_CHANGED;
+  hashManager.MAP_KEY = MAP_KEY;
   hashManager.updateHash = updateHash;
   hashManager.getState = getState;
   return hashManager;
