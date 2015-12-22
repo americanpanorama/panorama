f74560a7359e9fda8f5ec66317d28ee5b880d2cf
Seth Fitzsimmons <seth@mojodna.net>
2015-12-21 23:11:45 -0800

    Kitchen sink build (including externals)

---
+++ src/webpack.config.dist.full.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var path = require('path');
+var webpack = require('webpack');
+var merge = require('webpack-merge');
+var config = require('./webpack.config.js');
+
+module.exports = merge(config, {
+  externals: [],
+  output: {
+    path: path.join(__dirname, '..', 'dist'),
+    filename: 'components.full.js',
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      },
+      output: {
+        comments: false,
+        semicolons: true,
+      },
+    })
+  ]
+});
